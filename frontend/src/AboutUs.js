ec615183528e4d46ed9aaa544eb4888b6043f149
michaeltran9224 <michael.tran9224@gmail.com>
2023-11-30 04:22:00 -0800

    Testing React Routing

---
+++ frontend/src/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutUs() {
+    return(
+        <div>
+            <h1>About Us</h1>
+            
+        </div>
+    )
+}
+
+export default AboutUs;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,31 @@
-import React from 'react';
-import './App.css';
-function App() {
-  return (
-    <div className="App">
-      {/* Navigation Bar */}
-      <nav>
-        <div className="logo"></div>
-        <ul>
-          <li><a href="#home">Home</a> <a href="#features">Features</a><a href="#about">About Us</a> <a href="#contact">Developers</a></li>
-        </ul>
-      </nav>
-      {/* Hero Section */}
-      <header className="hero">
-        <div className="hero-content">
-          <h1></h1>
-          <p>.</p>
+import {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+
+//const API_URL = 
+
+const App = () => {
+    const [stockTicker, setStockTicker] = useState('');
+    const[currentStockValue, setCurrentStockValue] = useState('');
+    const [predictedStockValue, setPredictedStockValue] = useState('');
+
+    //handles user input
+    const handleInputChange = (e) => {
+        setStockTicker(e.target.value);
+        //use setstockticker to set value to the ticker
+        //add updates on currentstockvalue and predictedstock value with the ai script
+    };
+    return(
+        <div className="app">
+            <Routes>
+                <Route path ="/" element = {<App />}/>
+                <Route path ="/about" element = {<AboutUs />}/>
+                <Route path ="/contact" element = {<ContactUs />}/>
+            </Routes>
         </div>
-      </header>
-      {/* Features Section */}
-      <section id="features" className="features">
-        <h2>Features</h2>
-        <div className="feature">
-          <h3>Feature 1</h3>
-          <p>Description of Feature 1.</p>
-        </div>
-        <div className="feature">
-          <h3>Feature 2</h3>
-          <p>Description of Feature 2.</p>
-        </div>
-        {/* Add more features as needed */}
-      </section>
-      {/* About Us Section */}
-      <section id="about" className="about">
-        <h2>About Us</h2>
-        <p>
-          Brief description of your company or project. Explain what makes you unique.
-        </p>
-      </section>
-      {/* Contact Section */}
-      <section id="contact" className="contact">
-        <h2>Contact Us</h2>
-        <p>
-          Reach out to us for any inquiries or collaboration opportunities.
-        </p>
-        <form>
-          <label>Name:</label>
-          <input type="text" />
-          <label>Email:</label>
-          <input type="email" />
-          <label>Message:</label>
-          <textarea></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-      </section>
-      {/* Footer */}
-      <footer>
-        <p>&copy; 2023 Your Company. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+    );
+
 }
+
 export default App;
