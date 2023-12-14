f6f4dda234b73ed7a3cce802013b81ea304079aa
Abdul Kalam Syed <abdulksyed10@gmail.com>
2023-12-14 01:11:07 -0800

    Added tables finally

--- backend/index.py
+++ backend/index.py
@@ -21,6 +21,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 db = SQLAlchemy(app)
 
+""" CREATING TABLES """
+
+class User(db.Model):
+    __table_args__ = {'schema': 'stockpredict'}
+    user_id = db.Column(db.Integer, primary_key=True)    
+    email = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    ## This defines how the object is returned in string representation
+    def __repr__(self):
+        return f'<test id={self.user_id}, email={self.email}, name={self.name}, password={self.password} />'
+    
+class StockPredict(db.Model):
+    __table_args__ = {'schema': 'stockpredict'}
+    predict_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.user_id), nullable=False)
+    ticker = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    predicted_value = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<test predict_id={self.predict_id}, user_id={self.user_id}, ticker={self.ticker}, date={self.date}, timestamp={self.timestamp}, predicted_value={self.predicted_value} />'
+
+# creates all tables defined above. only run this if you're creating a new table.
+# with app.app_context():
+#     db.create_all()
+
 @app.route("/")
 def home():
     """
