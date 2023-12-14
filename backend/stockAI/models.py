8a963d5d728bbcb8295191c2be710eb85528da88
michaeltran9224 <michael.tran9224@gmail.com>
2023-12-13 21:39:29 -0800

    adding react routes

--- backend/stockAI/models.py
+++ backend/stockAI/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+def get_default_date():
+    return timezone.now().date()
 
 class StockPrediction(models.Model):
-    ticker = models.CharField(max_length=10)
-    start_date = models.DateField()
-    prediction = models.FloatField()
+    ticker = models.CharField(max_length=30, default='na')
+    startDate = models.DateField(default=get_default_date, blank=True)
+    predictedValue = models.CharField(max_length=30, default='na')
 
--- backend/stockAI/views.py
+++ backend/stockAI/views.py
@@ -9,7 +9,7 @@ from .models import StockPrediction
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from datetime import date
+from datetime import date, datetime
 from sklearn.model_selection import TimeSeriesSplit
 from sklearn.preprocessing import MinMaxScaler
 from keras.layers import LSTM, Dense
@@ -166,14 +166,33 @@ def predict(request, display_type="All", specified_ticker=""):
     # return render(request, "predict.html")
 
 class ReactView(APIView):
+    serializer_class = ReactSerializer
+
     def get(self, request):
-        output = [{"ticker": output.ticker,
-                    "startDate": output.startDate,
-                    "predictedValue": output.predictedValue}
-                    for output in StockPrediction.objects.all()]
-        return Response(output)
-    def post(self, request):
-        serializer = ReactSerializer(date=request.data)
-        if serializer.is_valid(raise_exception=True):
+        # Assuming 'ticker' and 'date' are sent as query parameters
+        ticker = request.query_params.get('ticker')
+        date = request.query_params.get('date')
+
+        # Filtering data based on ticker and date if they exist
+        if ticker:
+            output = StockPrediction.objects.filter(ticker=ticker)
+        else:
+            output = StockPrediction.objects.all()
+
+        if date:
+            try:
+                # Assuming the date is sent in 'yyyy-mm-dd' format from React
+                formatted_date = datetime.strptime(date, '%Y-%m-%d').date()
+                # Use formatted_date for further processing in Django
+            except ValueError:
+                # Handle incorrect date format sent from React
+                return Response({'error': 'Invalid date format. Expected yyyy-mm-dd.'}, status=400)
+
+        serializer = ReactSerializer(output, many=True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = ReactSerializer(data=request.data)
+        if serializer.is_valid(raise_exception = True):
             serializer.save()
             return Response(serializer.data)
