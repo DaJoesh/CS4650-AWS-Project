ed0193232bf310913d6f01b1dbd802f36b43a21f
michaeltran9224 <michael.tran9224@gmail.com>
2023-12-14 01:36:47 -0800

    starting react to flask connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,14 +16,24 @@ function App() {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />}/>
-                <Route path="/predictor" element={<Predictor />}/>
-                <Route path="/login" element={<Login />}/>
+                <Route path="/contact" element={<Contact />} />
+                <Route
+                    path="/predictor"
+                    element={isLoggedIn ? <Predictor /> : <Navigate to="/login" />}
+                />
+                <Route
+                    path="/login"
+                    element={<Login setIsLoggedIn={setIsLoggedIn} />}
+                />
             </Routes>
         </Router>
     );
 }
+
+/* The isLoggedIn state represents the user's login status. You should replace this with your actual authentication logic that determines whether the user is logged in.
+The Login component receives a function setIsLoggedIn as a prop. When the user successfully logs in within the Login component, you can call setIsLoggedIn(true) to update the login status.*/
+
  
 export default App;
--- frontend/src/pages/predictor.js
+++ frontend/src/pages/predictor.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
  
 const Predictor = () => {
     const [message, setMessage] = useState('');
@@ -7,10 +7,11 @@ const Predictor = () => {
     const [dateInput, setDateInput] = useState('');
 
     const handleSubmit = async (event) => {
-        console.log('sumbitted');
         event.preventDefault();
+        console.log('submitted');
         console.log(tickerInput);
         console.log(dateInput);
+
         try {
             const response = await fetch('http://127.0.0.1:5000/predict', {
                 method: 'POST',
@@ -30,23 +31,45 @@ const Predictor = () => {
                 setMessage('Failed to process data');
             }
 
-        setTickerInput('');
-        setDateInput('');
-    }
+            setTickerInput('');
+            setDateInput('');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
     useEffect(() => {
-        fetch('/predict').then(res=>res.json()).then(data => {
-            setTickerInput(data.predictedValue)
-        })
-    })
+        fetch('/predict')
+            .then(res => res.json())
+            .then(data => {
+                setTickerInput(data.predictedValue);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
     return (
         <div
             style={{
                 display: "flex",
-                justifyContent: "center",
+                flexDirection: 'column',
+                justifyContent: "flex-start",
                 alignItems: "center",
                 height: "100vh",
             }}
         >
+            <h1>
+                Stock Prediction
+            </h1>
+            <section>
+                    <h2>How To Use</h2>
+                    <ul>
+                        <li>Enter the 4 Letter Ticker into the Ticker box for the stock.</li>
+                        <li>Select the starting date from which data on this stock will be analyzed.</li>
+                        <li>Click submit to send the request to the Stock Predictor AI</li>
+                    </ul>
+                </section>
             <form onSubmit = {handleSubmit}>
                 <input
                     id="ticker_input"
@@ -74,9 +97,21 @@ const Predictor = () => {
                 </h2>
 
             </form>
-
+            <div style={{ display: 'flex' }}>
+                <ul>
+                    <li> prediction.ticker  -  prediction.prediction </li>
+                </ul>
+                <ul>
+                    <li> predictionordered.ticker  -  predictionordered.prediction </li>
+                </ul>
+                <ul>
+                    <li> predictionspecific.ticker  -  predictionspecific.prediction </li>
+                </ul>
+            </div>
         </div>
     );
-};
+}
+    
+
  
 export default Predictor;
